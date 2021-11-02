4954d20148b1e7fd327c4fd66740ac6d01a46058
Sahil Goyal <sahilgoyal8060@gmail.com>
2021-11-02 14:24:12 +0530

    Terraform init

---
+++ terraform/modules/vm/input.tf
@@ -0,0 +1,11 @@
+variable "location" {}
+variable "resource_group" {}
+
+variable "public_ip_address_id" {}
+variable "subnet_id" {}
+
+variable "admin_username" {}
+variable "admin_password" {}
+
+variable "webapp_name" {}
+variable "resource_type" {}
