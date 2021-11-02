01b8155a039d177defe4d2b8e2e5bb11a40a75ff
sahil8060 <72242460+sahil8060@users.noreply.github.com>
2021-11-02 16:06:30 +0530

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -21,10 +21,10 @@ resource "azurerm_linux_virtual_machine" "test" {
   disable_password_authentication = false
   network_interface_ids = [azurerm_network_interface.test.id]
 
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
+#  admin_ssh_key {
+#    username   = var.admin_username
+#    public_key = file("~/.ssh/id_rsa.pub")
+#  }
 
   os_disk {
     caching           = "ReadWrite"
